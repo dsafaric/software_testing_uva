008559df5bf80282ba6fa22710bb5db58f12aab5
Dominik Safaric <dsafaric@users.noreply.github.com>
2014-10-02 21:22:00 +0200

    Update Lab5.hs

--- Assignment-week-5/Lab5.hs
+++ Assignment-week-5/Lab5.hs
@@ -10,10 +10,10 @@ import Test.Hspec
 import System.Random
 import Test.QuickCheck
 import System.IO.Unsafe
-
+import SetOrd
 -- Exercise 1
 -- get the constraints function, and uniqueSolv 
--- you basically need to write a sudoko consistency check using Hspec
+-- you basically need to write a sudoko consistency check using HSpec
 -- the sudoko should always have unique solution (only one solution) and should always be consistent
 
 cons :: IO ()
@@ -36,4 +36,60 @@ genSud = do
 
 nonIO :: IO a -> a
 nonIO = \x -> unsafePerformIO x
+
+-- the following function can be used for testing so that cons won't test on the basis
+	-- of one sample only
+
+genRandomSudokus :: Int -> IO [Node]
+genRandomSudokus 0 = return []
+genRandomSudokus n = do
+		x <- genRandomSudoku
+		xs <- genRandomSudokus (n-1)
+		return (x:xs)
+
+-- Exercise 2
+-- every Sudoku problem P is minimal if it admits a unique solution
+
+-- solution 1
+minimal :: IO Node -> Bool
+minimal = uniqueSol . nonIO 	-- the function uses nonIO (IO Node) because of we want to test
+								-- the property on a random generated Sudoku problem
+								-- we are using the genSud function, althouth the predifined
+								-- genRandomSudoku could be used as well
+-- the tests are returning a true value of every input 
+
+-- solution 2
+
+randInt :: Int -> Int -> IO Int
+randInt = \x -> \y -> getStdRandom (randomR (x,y))
+
+randList :: IO [Int]
+randList = do 
+	g <- getStdGen
+	x <- randInt 10 20
+	return $ take x (randomRs (1,9) g)
+
+randCons :: [Int] -> [(Row, Column)]
+randCons [] = []
+randCons [x] = []
+randCons (x:y:xs) = (x,y) : randCons xs
+
+randCons' :: [(Row,Column)]
+randCons' = nub $ randCons (nonIO randList)
+
+-- node that was generated
+
+minimalHspec :: Node -> [(Row, Column)] -> IO ()
+minimalHspec n xs = hspec $ do 
+	describe "uniqueSol" $ do
+		it "Node has a unique solution"  $ do
+			(uniqueSol $ minimalize n xs) `shouldBe` (True :: Bool) 
+
+
+testProp :: IO ()
+testProp = minimalHspec (nonIO genRandomSudoku) randCons'
+-- the idea was to either use a list of nodes or a list of constraints to minimalize nodes to a level of 
+	-- minimalness.. But that unfortunally doesn't work with Hspec since applying it to a list
+	-- of objects is difficult because it accepts only the Testable objects -  while Sudoko isn't
+
 
