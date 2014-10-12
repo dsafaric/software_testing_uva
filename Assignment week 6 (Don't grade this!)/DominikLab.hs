217a19995bfed650a8d4ac9a31ca8cca95b3acad
Dominik Safaric <dsafaric@users.noreply.github.com>
2014-10-12 14:09:19 +0200

    Update DominikLab.hs

--- Assignment week 6 (Don't grade this!)/DominikLab.hs
+++ Assignment week 6 (Don't grade this!)/DominikLab.hs
@@ -125,6 +125,13 @@ testPropF = do
 -- Exercise 5
 -- time spent: 10 min  
 
+carmichael :: [Integer]
+carmichael = [ (6*k+1)*(12*k+1)*(18*k+1) | 
+      k <- [2..], 
+      isPrime (6*k+1), 
+      isPrime (12*k+1), 
+      isPrime (18*k+1) ]
+
 testPropC :: IO ()
 testPropC = do
 	k <- randInt 1 3
